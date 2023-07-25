616acea425bc760730ea5d539659ac772d0d2b97
Chrysostomos Valsamakis <93266799+ChrisostomosVals@users.noreply.github.com>
2023-07-25 10:17:06 +0300

    Update Program.cs

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddCors();
 builder.Services.AddOcelot(builder.Configuration);
 builder.WebHost.ConfigureKestrel(options =>
 {
